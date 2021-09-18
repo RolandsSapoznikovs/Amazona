7897adbe007c24be8924f10cdeb7305219005952
Rolands Sapoznikovs <rolands_sapoznikovs@inbox.lv>
2021-09-18 15:48:36 +0300

    Amazona

---
+++ frontend/src/data.js
@@ -0,0 +1,51 @@
+const data = {
+    product: [
+        {
+            _id: '1',
+            name: 'Red Shirt',
+            category: 'Shirts',
+            image: 'https://www.marni.com/12/12386489MT_13_n_r.jpg',
+            price: 120,
+            brand: 'Adidas',
+            description: 'What the dog doin?',
+        },
+        {
+            _id: '2',
+            name: 'Gucci Red Shirt',
+            category: 'Shirts',
+            image: 'https://www.marni.com/12/12386489MT_13_n_r.jpg',
+            price: 9001,
+            brand: 'Gucci',
+            description: 'What the dog doin?',
+        },
+        {
+            _id: '3',
+            name: 'Ok Red Shirt',
+            category: 'Shirts',
+            image: 'https://www.marni.com/12/12386489MT_13_n_r.jpg',
+            price: 60,
+            brand: 'Abibas',
+            description: 'What the dog doin?',
+        },
+        {
+            _id: '4',
+            name: 'Roze Shirt',
+            category: 'Shirts',
+            image: 'https://www.marni.com/12/12386489MT_13_n_r.jpg',
+            price: 2,
+            brand: 'Chineese people',
+            description: 'What the dog doin?',
+        },
+        {
+            _id: '5',
+            name: 'Actually not a shirt, dont buy plz',
+            category: 'Shirts',
+            image: 'https://www.marni.com/12/12386489MT_13_n_r.jpg',
+            price: 1000000,
+            brand: 'Guc',
+            description: 'What the dog doin?',
+        },
+
+    ]
+}
+export default data;
